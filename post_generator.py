53940ecb2fe8d02e6dd91bb0c8916d8ede927dee
Tracy <tracy.anele@pictureworks.com.au>
2024-06-16 17:00:10 +0100

    model update

--- post_generator.py
+++ post_generator.py
@@ -1,6 +1,6 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-model_name = 'gpt2'
+model_name = 'distilgpt2'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
